a146869f9ad2293dc6629d3761e9e3af962cddbb
AyanSwarga <ayannicholas@gmail.com>
2023-05-17 21:46:57 +0100

    task1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 ----This script lists all privileges of the MySQL users user_0d_1 
 ----and user_0d_2 on my server in localhost. 
 
-SHOW GRANT FOR ('user_0d_1', 'user_0d_2') AND host = 'localhost';
+SHOW GRANT FOR ('user_0d_1', 'user_0d_2') IN 'localhost';
 
