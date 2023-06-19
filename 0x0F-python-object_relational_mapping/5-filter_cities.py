e2951be0fcba3dee3f206517acdef08f5427afd6
AyanSwarga <ayannicholas@gmail.com>
2023-06-19 10:06:27 +0100

    python orm

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""This script  takes in the name of a state
+""" This script  takes in the name of a state
 as an argument and lists all cities of that
 state, using the database `hbtn_0e_4_usa`.
 """
@@ -10,7 +10,7 @@ from sys import argv
 
 if __name__ == "__main__":
     
-    """Access to the database and get the cities
+    """ Access to the database and get the cities
     from the database.
     """
 
